a0ca2f22dfc3003dd54328d6780f0308da160e5b
DS_RakeshKumar <rakesh.hr@dyashin.com>
2025-09-05 16:42:54 +0530

    crated api to add movie rating

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,11 +1,17 @@
 const Movie = require("../models/Movies");
+const Genre = require("../models/Genres");
 
 const addMovie = async (req, res) => {
   try {
     const { title, synopsis, cover, genres } = req.body;
 
-    if (!title || !synopsis || !cover) {
+    if (!title || !synopsis || !cover || !genres || !genres.length) {
       return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const findGenres = await Genre.find({ _id: { $in: genres } });
+    if (findGenres.length !== genres.length) {
+      return res.status(400).json({ message: "Invalid genre" });
     }
 
     const movie = new Movie({
@@ -25,7 +31,7 @@ const addMovie = async (req, res) => {
 
 const getMovies = async (req, res) => {
   try {
-    const movies = await Movie.find();
+    const movies = await Movie.find().populate("genres", "title");
     res.json(movies);
   } catch (error) {
     console.error(error);
@@ -33,4 +39,33 @@ const getMovies = async (req, res) => {
   }
 };
 
-module.exports = { addMovie, getMovies };
+const addRating = async (req, res) => {
+  try {
+    const { movieId, rating } = req.body;
+
+    if (!movieId || rating == null) {
+      return res
+        .status(400)
+        .json({ message: "Movie ID and rating are required" });
+    }
+
+    const movie = await Movie.findById(movieId);
+    if (!movie) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+
+    movie.ratings.push(rating);
+    movie.reviewsCount = movie.ratings.length;
+    movie.averageRating =
+      movie.ratings.reduce((acc, curr) => acc + curr, 0) / movie.ratings.length;
+
+    await movie.save();
+
+    res.json({ message: "Rating added", movie });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { addMovie, getMovies, addRating };
